974f9a309207768b112597ea264e8f7f356353c4
bullimog <gmb@bullimore.com>
2015-06-14 12:27:40 +0100

    clean up

--- test/SequencerSpec.scala
+++ test/SequencerSpec.scala
@@ -41,13 +41,13 @@ class SequencerSpec extends Specification {
   val thermos = List(thermo)
   val componentCollection = ComponentCollection ("Masher 1", "My first set-up, for mashing", devices, thermos)
 
-  val stepDefn1 = Step(104, Step.SET_HEAT, Some(41), None)      // Set required thermostat temp to 41
+  val stepDefn1 = Step(104, Step.SET_HEAT, Some(41), None)    // Set required thermostat temp to 41
   val stepDefn2 = Step(2, Step.ON, None, None)                // Turn pump on
   val stepDefn3 = Step(1, Step.WAIT_HEAT, Some(41), None)     // Wait for thermometer to reach 41
-  val stepDefn4 = Step(104, Step.WAIT_TIME, None, Some(5))  // wait for 5 seconds
-  val stepDefn5 = Step(104, Step.SET_HEAT, Some(68), None)      // Set thermostat temp to 68
-  val stepDefn6 = Step(104, Step.WAIT_TIME, None, Some(5))  // wait for 5 seconds
-  val stepDefn7 = Step(104, Step.OFF, None, None)               // turn boiler off
+  val stepDefn4 = Step(104, Step.WAIT_TIME, None, Some(5))    // wait for 5 seconds
+  val stepDefn5 = Step(104, Step.SET_HEAT, Some(68), None)    // Set thermostat temp to 68
+  val stepDefn6 = Step(104, Step.WAIT_TIME, None, Some(5))    // wait for 5 seconds
+  val stepDefn7 = Step(104, Step.OFF, None, None)             // turn boiler off
   val stepDefn8 = Step(2, Step.OFF, None, None)               // turn pump off
 
   val lb2 = new ListBuffer[Step]()
